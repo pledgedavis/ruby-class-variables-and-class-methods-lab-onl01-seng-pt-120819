936a9a920c83ba70c7b9e3ec21957b283580b318
pledgedavis <pledgedavis14@gmail.com>
2020-01-11 20:28:55 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,66 @@
+class Song
+  # attr_reader :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+# @@genre_count = {}
+# @@artist_count = {}
+ def initialize( name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+     @@genres << genre
+     @@artists << artist
+  end
+  
+  def self.count
+     @@count
+  # @@artist
+  end
+
+  def self.artists
+     @@artists.uniq!
+  end
+  
+  def self.genres
+    @@genres.uniq!
+  end
+  
+  def name 
+    @name
+  end
+ 
+  def artist
+     @artist
+   end
+ 
+  def genre
+     @genre
+  end
+ 
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+          genre_count[genre]+=1
+      else
+          genre_count[genre]=1
+      end
+    end
+    genre_count
+ end
+ 
+  def self.artist_count
+         artist_count = {}
+          @@artists.each do |artist|
+         if artist_count[artist]
+            artist_count[artist] +=1
+         else 
+            artist_count[artist] =1
+         end
+         end
+         artist_count
+  end
+end
+
