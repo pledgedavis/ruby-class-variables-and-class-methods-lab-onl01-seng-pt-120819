a4b13b82f69fcfb48fdde508ae9faf2968812c97
pledgedavis <pledgedavis14@gmail.com>
2020-01-11 19:17:40 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+  # attr_reader :name, :artist, :genre
+@@count = 0
+@@genre = []
+@@artists = []
+# @@artists
+def initialize( name, artist, genre)
+@name = name
+@artist = artist
+@genre = genre
+@@count += 1
+@@genre = genre
+@@artist = artist
+end
+def self.count
+  @@count
+  # @@artist
+end
+ def name 
+   @name
+ end
+def artist
+  @artist
+end
+def genre
+  @genre
+end
+
+
+
+end
+# end
+
