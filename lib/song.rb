b15725ef39e796f72b87851e31bbcce151016305
pledgedavis <pledgedavis14@gmail.com>
2020-01-11 20:24:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,9 +55,9 @@ class Song
          artist_count = {}
           @@artists.each do |artist|
          if artist_count[artist]
-            artist_count[artist]+=1
+            artist_count[artist] +=1
          else 
-            artist_count[artist]=1
+            artist_count[artist] =1
          end
          artist_count
          end
