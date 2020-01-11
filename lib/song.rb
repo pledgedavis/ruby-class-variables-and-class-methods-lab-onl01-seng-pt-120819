a56c30e18d8430a74d097e07ad9c847363224e06
pledgedavis <pledgedavis14@gmail.com>
2020-01-11 20:12:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,21 +6,24 @@ class Song
 # @@genre_count = {}
 # @@artist_count = {}
  def initialize( name, artist, genre)
-@name = name
-@artist = artist
-@genre = genre
-@@count += 1
- @@genres << genre
- @@artists << artist
-end
-def self.count
-  @@count
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+     @@genres << genre
+     @@artists << artist
+  end
+  
+  def self.count
+     @@count
   # @@artist
-end
-def self.artists
-  @@artists.uniq!
-end
-def self.genres
+  end
+
+  def self.artists
+     @@artists.uniq!
+  end
+  
+  def self.genres
 @@genres.uniq!
 end
 def name 
