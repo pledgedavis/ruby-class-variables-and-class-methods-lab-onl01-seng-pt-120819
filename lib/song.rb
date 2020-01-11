d5047ee63bfa55355787ba0ddfe4d60806b9d614
pledgedavis <pledgedavis14@gmail.com>
2020-01-11 19:32:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,9 +3,8 @@ class Song
 @@count = 0
 @@genres = []
 @@artists = []
-@@
-@@artist_count ={}
-@@
+@@genre_count = {}
+@@artist_count = {}
 def initialize( name, artist, genre)
 @name = name
 @artist = artist
