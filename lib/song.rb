9bf79a4b2a6ad45417c6969e5679286f33409e8b
pledgedavis <pledgedavis14@gmail.com>
2020-01-11 20:14:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,25 +27,29 @@ class Song
     @@genres.uniq!
   end
   
-def name 
-   @name
- end
- def artist
-   @artist
- end
- def genre
-   @genre
- end
-def self.genre_count
-  genre_count = {}
-  @@genres.each do |genre|
-   if genre_count[genre]
-     genre_count[genre]+=1
-   else
-     genre_count[genre]=1
+  def name 
+    @name
   end
-  genre_count
-end
+ 
+  def artist
+     @artist
+   end
+ 
+  def genre
+     @genre
+  end
+ 
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+          genre_count[genre]+=1
+      else
+          genre_count[genre]=1
+      end
+    end
+    genre_count
+ end
 #     def self.artist_count
 #       @@artist_count.each do |artists|
 #         @@artist_count[artists]
